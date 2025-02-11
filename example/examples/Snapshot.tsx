a98e4bf29b100c92aa6a95f4c543e5b958e9c4e9
Adam Love <adam.love127@gmail.com>
2025-02-11 12:42:17 +1100

    Handle image compression and scaling

--- example/examples/Snapshot.tsx
+++ example/examples/Snapshot.tsx
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, PanResponder, Platform, Button } from 'react-native';
-
-// import WebView, {WebViewSnapshotEvent} from 'react-native-webview';
+import { View, Button } from 'react-native';
 import WebView, { WebViewSnapshotEvent } from '../../src/WebView';
 
-type Props = {};
-type State = {};
+// TODO: move this event elsewhere
 
-export default class Snapshot extends Component<Props, State> {
-  state = {};
+// import WebView from 'react-native-webview';
 
+export default class Snapshot extends Component {
   constructor(props) {
     super(props);
-    this.webView = React.createRef();
     this.state = {
-      text: 'Not Started',
+      webViewHeight: 240, // store the initial height in state
     };
+    this.webView = React.createRef();
   }
 
   snapshot = () => {
-    this.webView.current.takeSnapshot('foo11.png');
+    if (!this.webView.current?.takeSnapshot) {
+      throw new Error('takeSnapshot is not defined');
+    }
+
+    // Increase the height by 20 before taking a snapshot.
+    this.setState({ webViewHeight: 1200 }, () => {
+      // After state has been updated, call takeSnapshot.
+      setTimeout(async () => {
+        console.log('In timeout');
+        const res = await this.webView.current.takeSnapshot('foo11.png');
+        console.log('res', res);
+      }, 300);
+    });
   };
 
   onSnapShotCreated = ({ nativeEvent: event }: { nativeEvent: WebViewSnapshotEvent }) => {
     console.log('onSnapShotCreated', event);
+    this.setState({ webViewHeight: 240 });
   };
 
   render() {
+    const { webViewHeight } = this.state;
     return (
-      <View style={{ height: 240 }}>
+      <View style={{ height: webViewHeight }}>
         <WebView
           ref={this.webView}
           source={{ url: 'https://www.google.com' }}
